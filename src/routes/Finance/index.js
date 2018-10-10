c27c9635bdb692c65eb81bf7c2c1064b6aeb4f11
barnettZQG <zengqg@goodrain.com>
2018-10-10 15:11:09 +0800

    [REV] update version

--- src/routes/Finance/index.js
+++ src/routes/Finance/index.js
@@ -322,7 +322,7 @@ export default class BasicList extends PureComponent {
               {this.props.enterprise && this.props.enterprise.create_time}
             </Description>
             <Description term="平台版本">
-              {this.props.rainbondInfo.version || "V3.7.1-release"}
+              {this.props.rainbondInfo.version || "V3.7.2-release"}
               <Button type="primary" style={{ marginLeft: 16 }} onClick={this.handelUnderstand}>
                 了解企业解决方案
               </Button>
