78c662b556dd8902559caf3f44b4e01e27b15438
wangch <wangc@goodrain.com>
2019-01-15 14:07:55 +0800

    [REV] fix bug with 592 and formate code

--- src/components/ImportBackup/index.js
+++ src/components/ImportBackup/index.js
@@ -1,99 +1,121 @@
-import React, {PureComponent} from 'react';
-import {connect} from 'dva';
-import {Link, Switch, Route, routerRedux} from 'dva/router';
+import React, { PureComponent } from "react";
+import { connect } from "dva";
+import { Link, Switch, Route, routerRedux } from "dva/router";
 import {
-	Row,
-	Col,
-	Card,
-	Form,
-	Button,
-	Icon,
-	notification,
-	Modal,
-	Upload
-} from 'antd';
-import globalUtil from '../../utils/global';
-import config from '../../config/config';
-import cookie from '../../utils/cookie';
+  Row,
+  Col,
+  Card,
+  Form,
+  Button,
+  Icon,
+  notification,
+  Modal,
+  Upload,
+  message
+} from "antd";
+import globalUtil from "../../utils/global";
+import config from "../../config/config";
+import cookie from "../../utils/cookie";
 
-const token = cookie.get('token');
-let myheaders = {}
+const token = cookie.get("token");
+let myheaders = {};
 if (token) {
-   myheaders.Authorization = `GRJWT ${token}`;  
-   myheaders['X_REGION_NAME'] = globalUtil.getCurrRegionName();
-   myheaders['X_TEAM_NAME'] = globalUtil.getCurrTeamName();
+  myheaders.Authorization = `GRJWT ${token}`;
+  myheaders["X_REGION_NAME"] = globalUtil.getCurrRegionName();
+  myheaders["X_TEAM_NAME"] = globalUtil.getCurrTeamName();
 }
 
 // @connect(({user, global}) => ({currUser: user.currentUser}))
 
-@connect(({user,global,groupControl}) => ({groupDetail: groupControl.groupDetail || {},currUser: user.currentUser,groups: global.groups || []}))
+@connect(({ user, global, groupControl }) => ({
+  groupDetail: groupControl.groupDetail || {},
+  currUser: user.currentUser,
+  groups: global.groups || []
+}))
 export default class Index extends PureComponent {
-    constructor(props){
-      super(props);
-      this.state={
-        fileList: []
-      }
-    }
-   
-    onChange = (info) => {
-      let fileList = info.fileList;
-      fileList = fileList.filter((file) => {
-        if (file.response) {
-          return file.response.msg === 'success';
-        }
-        return true;
-      });
-  
-      this.setState({ fileList },()=>{
-        const file = this.state.fileList
-        if(file.length > 0){
-          console.log(file[0])
-          console.log(file[0].response)
-          if(file[0].status == 'done'){
-            this.props.onReLoad && this.props.onReLoad();
-          }else{
-            console.log("111111")
-          }
-        }
-      });
-    }
-	onRemove =()=>{
-    notification.info({
-      message: '备份已导入',
-      duration:'2'
-    });
-		return false
-	}
-  onData = (e)=>{
-     console.log(e)
+  constructor(props) {
+    super(props);
+    this.state = {
+      fileList: []
+    };
   }
-    render(){
-	  const group_id = this.props.groupId;
-      const team_name = globalUtil.getCurrTeamName();
-	  const uploadUrl = config.baseUrl + '/console/teams/'+ team_name +'/groupapp/'+ group_id +'/backup/import';
-      const fileList = this.state.fileList;
-      
-      return (
-         <Modal
-           visible={true}
-           onCancel={this.props.onCancel}
-           title="请导入备份"
-		   footer={
-			[<Button key="back" onClick={this.props.onCancel}>关闭</Button>]
-			}
-         >
-            <Upload 
-               action={uploadUrl}
-               fileList={fileList}
-               onChange={this.onChange}
-			        headers = {myheaders}
-              onRemove ={this.onRemove}
-              data={this.onData}
-            >
-                
-                {fileList.length > 0? null: <Button>请选择文件</Button>}
-            </Upload>
-         </Modal>
-      )
+
+  onChange = info => {
+    let { fileList } = info;
+    const status = info.file.status;
+    if (status === "done") {
+      this.props.onReLoad && this.props.onReLoad();
+    } else if (status === "error") {
+      const { response } = info.file;
+      notification.error({ message: response.msg_show });
     }
+
+    //重新设置state
+    if (fileList[0] && fileList[0].status == "error") {
+      this.setState({ fileList: [] });
+      return;
+    } else {
+      this.setState({ fileList });
+    }
+  };
+  //处理进度条
+  handleProgress = (event, file) => {
+    let { fileList } = this.state;
+    fileList = fileList.map(item => {
+      if (item.uid === file.uid) {
+        item.percent = event.percent;
+      }
+      return item;
+    });
+    this.setState({ fileList });
+  };
+  onRemove = file => {
+    notification.info({
+      message: "备份已导入",
+      duration: "2"
+    });
+    return false;
+    // this.setState((state) => {
+    //   const index = state.fileList.indexOf(file);
+    //   const newFileList = state.fileList.slice();
+    //   newFileList.splice(index, 1);
+    //   return {
+    //     fileList: newFileList,
+    //   };
+    // });
+  };
+  onData = file => {
+    console.log("--->" + JSON.stringify(file));
+  };
+  render() {
+    const group_id = this.props.groupId;
+    const team_name = globalUtil.getCurrTeamName();
+    const uploadUrl = config.baseUrl + "/console/teams/" + team_name + "/groupapp/" + group_id + "/backup/import";
+    const fileList = this.state.fileList;
+
+    return (
+      <Modal
+        visible={true}
+        onCancel={this.props.onCancel}
+        title="请导入备份"
+        footer={[
+          <Button key="back" onClick={this.props.onCancel}>
+            关闭
+          </Button>
+        ]}
+      >
+        <Upload
+          action={uploadUrl}
+          fileList={fileList}
+          onChange={this.onChange}
+          headers={myheaders}
+          onRemove={this.onRemove}
+          onProgress={this.handleProgress}
+          data={this.onData}
+        >
+          {fileList.length > 0 ? null : <Button>请选择文件</Button>}
+        </Upload>
+      </Modal>
+    );
+  }
 }
