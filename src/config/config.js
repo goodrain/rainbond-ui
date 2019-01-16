9ce78b1bae2c48b05c926e3e6d359535e3740697
wangch <wangc@goodrain.com>
2019-01-16 11:12:00 +0800

    [REV] Determine if the team the current user is on is an administrator to handle the link jump

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === "dev") {
   baseUrl = "http://gr-debug.goodrain.com/";
 } else if (process.env.NODE_ENV === "development") {
   // baseUrl = '/api';
-  baseUrl = "http://dev.goodrain.org";
+  baseUrl = "http://47.93.218.143:7070";
   // baseUrl = "http://39.96.22.211:7070";
 } else if (process.env.NODE_ENV === "production") {
   baseUrl = "";
