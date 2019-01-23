fd5330ed4c9684eef39a64751e5bfbedea894c3b
ysicing zheng <i@ysicing.me>
2019-01-23 17:03:03 +0800

    [ADD] fix travis ci

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VERSION=3.7.2
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker build -t rainbond/rainbond-ui:$VERSION .
+docker push rainbond/rainbond-ui:$VERSION
+sed -i "s/VERSION/$VERSION/g" ./build/Dockerfile
+mv dist build/dist
+docker build -t rainbond/rbd-app-ui:$VERSION ./build
+docker push rainbond/rbd-app-ui:$VERSION
