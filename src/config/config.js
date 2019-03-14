53733832913846beeed91a9eb424033f2d9eb8fc
barnettZQG <zengqg@goodrain.com>
2019-03-14 15:03:24 +0800

    add demo team limit

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "dev") {
   // baseUrl = "http://dev.goodrain.org";
   // baseUrl = "http://39.104.87.151:7070";
   // baseUrl = "http://39.104.160.112:7070";
-  baseUrl = "http://192.168.1.200:7070";
+  baseUrl = "http://39.104.78.149:7070";
  
 } else if (process.env.NODE_ENV === "production") {
   baseUrl = "";
