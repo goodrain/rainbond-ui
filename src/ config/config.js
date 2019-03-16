0929ffe4eec4251d0814eb3db2ac18ed6cdc2fe4
zsl1549 <32888522+zsl1549@users.noreply.github.com>
2019-03-16 23:06:33 +0800

    Update and rename src/config/config.js to  src/ config/config.js

--- src/ config/config.js
+++ src/ config/config.js
@@ -4,8 +4,16 @@ if (process.env.NODE_ENV === "dev") {
   baseUrl = "http://gr-debug.goodrain.com/";
 } else if (process.env.NODE_ENV === "development") {
   // baseUrl = '/api';
-  baseUrl = "http://192.168.1.200:7070";
+  // baseUrl = "http://dev.goodrain.org";
+  // baseUrl = "http://39.104.87.151:7070";
+  // baseUrl = "http://39.104.160.112:7070";
+  // baseUrl = "http://192.168.1.200:7070";
+//   baseUrl = "http://console.goodrain.com";
   // baseUrl = "http://39.104.24.43:7070";
+  baseUrl = "http://39.104.24.43:7070";
+
+  
+ 
 } else if (process.env.NODE_ENV === "production") {
   baseUrl = "";
 }
