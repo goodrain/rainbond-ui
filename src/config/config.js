acbc5073587476faa8675d7d3667efb6f0b9f41e
张士龙 <zsl1549@163.com>
2019-03-16 23:21:26 +0800

    [REV] Modify the  Fix adding 2019-03-16

---
+++ src/config/config.js
@@ -0,0 +1,26 @@
+let baseUrl = "";
+let imageUploadUrl = "";
+if (process.env.NODE_ENV === "dev") {
+  baseUrl = "http://gr-debug.goodrain.com/";
+} else if (process.env.NODE_ENV === "development") {
+  // baseUrl = '/api';
+  // baseUrl = "http://dev.goodrain.org";
+  // baseUrl = "http://39.104.87.151:7070";
+  // baseUrl = "http://39.104.160.112:7070";
+  // baseUrl = "http://192.168.1.200:7070";
+  baseUrl = "http://console.goodrain.com";
+  // baseUrl = "http://39.104.24.43:7070";
+  // baseUrl = "http://39.104.24.43:7070";
+
+  
+ 
+} else if (process.env.NODE_ENV === "production") {
+  baseUrl = "";
+}
+
+imageUploadUrl = `${baseUrl}/console/files/upload`;
+const config = {
+  baseUrl,
+  imageUploadUrl,
+};
+export default config;
