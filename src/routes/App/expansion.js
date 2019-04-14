234c9090c0c9d2a55173577445c65fcec9a8cf21
barnettZQG <zengqg@goodrain.com>
2019-04-14 10:20:27 +0800

    add grctl service get cmd copy way

--- src/routes/App/expansion.js
+++ src/routes/App/expansion.js
@@ -1,7 +1,19 @@
 import React, { PureComponent } from "react";
 import { connect } from "dva";
 import { routerRedux } from "dva/router";
-import { Row, Col, Card, Form, Button, Select, notification, Spin } from "antd";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import {
+  Row,
+  Col,
+  Card,
+  Form,
+  Button,
+  Select,
+  notification,
+  Spin,
+  Divider,
+  Input
+} from "antd";
 import sourceUtil from "../../utils/source";
 import { horizontal, vertical } from "../../services/app";
 import globalUtil from "../../utils/global";
@@ -10,13 +22,19 @@ import NoPermTip from "../../components/NoPermTip";
 import InstanceList from "../../components/AppInstanceList";
 
 const { Option } = Select;
+const { Search } = Input;
 
-@connect(({ user, appControl }) => ({
-  currUser: user.currentUser,
-  baseInfo: appControl.baseInfo,
-  extendInfo: appControl.extendInfo,
-  instances: appControl.pods,
-}), null, null, { pure: false, withRef: true })
+@connect(
+  ({ user, appControl }) => ({
+    currUser: user.currentUser,
+    baseInfo: appControl.baseInfo,
+    extendInfo: appControl.extendInfo,
+    instances: appControl.pods
+  }),
+  null,
+  null,
+  { pure: false, withRef: true }
+)
 @Form.create()
 export default class Index extends PureComponent {
   constructor(arg) {
@@ -33,11 +51,11 @@ export default class Index extends PureComponent {
       this.setState({
         instances: nextProps.instances,
         loading: false
-      })
+      });
     } else {
       this.setState({
         loading: false
-      })
+      });
     }
   }
   componentDidMount() {
@@ -58,8 +76,8 @@ export default class Index extends PureComponent {
     vertical({
       team_name: globalUtil.getCurrTeamName(),
       app_alias: this.props.appAlias,
-      new_memory: memory,
-    }).then((data) => {
+      new_memory: memory
+    }).then(data => {
       if (data && !data.status) {
         notification.success({ message: "操作成功，执行中" });
       }
@@ -70,8 +88,8 @@ export default class Index extends PureComponent {
     horizontal({
       team_name: globalUtil.getCurrTeamName(),
       app_alias: this.props.appAlias,
-      new_node: node,
-    }).then((data) => {
+      new_node: node
+    }).then(data => {
       if (data && !data.status) {
         notification.success({ message: "操作成功，执行中" });
       }
@@ -81,35 +99,33 @@ export default class Index extends PureComponent {
     let adPopup = window.open("about:blank");
     const appAlias = this.props.appAlias;
     if (podName && manageName) {
-      this
-        .props
-        .dispatch({
-          type: "appControl/managePod",
-          payload: {
-            team_name: globalUtil.getCurrTeamName(),
-            app_alias: appAlias,
-            pod_name: podName,
-            manage_name: manageName,
-          },
-          callback: () => {
-            adPopup.location.href = `/console/teams/${globalUtil.getCurrTeamName()}/apps/${appAlias}/docker_console/`;
-          },
-        });
+      this.props.dispatch({
+        type: "appControl/managePod",
+        payload: {
+          team_name: globalUtil.getCurrTeamName(),
+          app_alias: appAlias,
+          pod_name: podName,
+          manage_name: manageName
+        },
+        callback: () => {
+          adPopup.location.href = `/console/teams/${globalUtil.getCurrTeamName()}/apps/${appAlias}/docker_console/`;
+        }
+      });
     }
-  }
+  };
   fetchExtendInfo = () => {
     const { dispatch } = this.props;
     dispatch({
       type: "appControl/fetchExtendInfo",
       payload: {
         team_name: globalUtil.getCurrTeamName(),
-        app_alias: this.props.appAlias,
+        app_alias: this.props.appAlias
       },
-      handleError: (res) => {
+      handleError: res => {
         if (res && res.status === 403) {
           this.props.dispatch(routerRedux.push("/exception/403"));
         }
-      },
+      }
     });
   };
   fetchInstanceInfo = () => {
@@ -118,22 +134,21 @@ export default class Index extends PureComponent {
       type: "appControl/fetchPods",
       payload: {
         team_name: globalUtil.getCurrTeamName(),
-        app_alias: this.props.appAlias,
+        app_alias: this.props.appAlias
       },
-      callback: (res) => {
-        if (res&&res._code == 200) {
+      callback: res => {
+        if (res && res._code == 200) {
           this.setState({
             instances: res.list,
             loading: false
-          })
+          });
         } else {
           this.setState({
             loading: false
-          })
+          });
         }
       }
-    })
-
+    });
   };
 
   render() {
@@ -144,25 +159,69 @@ export default class Index extends PureComponent {
     if (!extendInfo) {
       return null;
     }
+    const grctlCmd =
+      "grctl service get " +
+      this.props.appAlias +
+      " -t " +
+      globalUtil.getCurrTeamName();
     return (
       <div>
         <Card
           title="实例情况"
           extra={
-            <a onClick={() => {
-              this.setState({
-                loading: true
-              }, () => {
-                this.fetchInstanceInfo()
-              })
-            }} href="javascript:;">
+            <a
+              onClick={() => {
+                this.setState(
+                  {
+                    loading: true
+                  },
+                  () => {
+                    this.fetchInstanceInfo();
+                  }
+                );
+              }}
+              href="javascript:;"
+            >
               刷新
             </a>
           }
         >
-          {loading ? <Spin tip="Loading..."><div style={{ minHeight: "190px" }}></div></Spin> :
-            <InstanceList handlePodClick={this.handlePodClick} list={this.state.instances} />
-          }
+          {loading ? (
+            <Spin tip="Loading...">
+              <div style={{ minHeight: "190px" }} />
+            </Spin>
+          ) : (
+            <div>
+              <InstanceList
+                handlePodClick={this.handlePodClick}
+                list={this.state.instances}
+              />
+              <Divider />
+              <div>
+                <Row>
+                  <Col span={12}>
+                    <span style={{lineHeight: "32px"}}>
+                      查询详细的服务实例信息，请复制以下查询命令到Rainbond管理节点查询：
+                    </span>
+                  </Col>
+                  <Col span={12}>
+                    <Input 
+                        style={{width: "70%"}}
+                        value={grctlCmd}
+                    />
+                    <CopyToClipboard
+                      text={grctlCmd}
+                      onCopy={() => {
+                        notification.success({ message: "复制成功" });
+                      }}
+                    >
+                      <Button type="primary" style={{width: "25%", marginLeft: 16}}>复制查询命令</Button>
+                    </CopyToClipboard>
+                  </Col>
+                </Row>
+              </div>
+            </div>
+          )}
         </Card>
         <Card style={{ marginTop: 16 }} title="手动伸缩">
           <Row gutter={16}>
@@ -170,15 +229,25 @@ export default class Index extends PureComponent {
               <Form layout="inline" hideRequiredMark>
                 <Form.Item label="内存">
                   {getFieldDecorator("memory", {
-                    initialValue: `${extendInfo.current_memory}`,
-                  })(<Select
-                    style={{
-                      width: 200,
-                    }}
+                    initialValue: `${extendInfo.current_memory}`
+                  })(
+                    <Select
+                      style={{
+                        width: 200
+                      }}
+                    >
+                      {(extendInfo.memory_list || []).map(item => (
+                        <Option key={item} value={item}>
+                          {sourceUtil.getMemoryAndUnit(item)}
+                        </Option>
+                      ))}
+                    </Select>
+                  )}{" "}
+                  <Button
+                    onClick={this.handleVertical}
+                    size="default"
+                    type="primary"
                   >
-                    {(extendInfo.memory_list || []).map(item => <Option key={item} value={item}>{sourceUtil.getMemoryAndUnit(item)}</Option>)}
-                  </Select>)}{" "}
-                  <Button onClick={this.handleVertical} size="default" type="primary">
                     设置
                   </Button>
                 </Form.Item>
@@ -187,14 +256,26 @@ export default class Index extends PureComponent {
             <Col lg={12} md={12} sm={24}>
               <Form layout="inline" hideRequiredMark>
                 <Form.Item label="实例数量">
-                  {getFieldDecorator("node", { initialValue: extendInfo.current_node })(<Select
-                    style={{
-                      width: 200,
-                    }}
+                  {getFieldDecorator("node", {
+                    initialValue: extendInfo.current_node
+                  })(
+                    <Select
+                      style={{
+                        width: 200
+                      }}
+                    >
+                      {(extendInfo.node_list || []).map(item => (
+                        <Option key={item} value={item}>
+                          {item}
+                        </Option>
+                      ))}
+                    </Select>
+                  )}{" "}
+                  <Button
+                    onClick={this.handleHorizontal}
+                    size="default"
+                    type="primary"
                   >
-                    {(extendInfo.node_list || []).map(item => <Option key={item} value={item}>{item}</Option>)}
-                  </Select>)}{" "}
-                  <Button onClick={this.handleHorizontal} size="default" type="primary">
                     设置
                   </Button>
                 </Form.Item>
