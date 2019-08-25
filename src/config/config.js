e8b3c402fbee638b4b34a120c71f14515eb961bf
张士龙 <zsl1549@163.com>
2019-08-26 00:17:56 +0800

    [REV] Modify the the style

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 let baseUrl = "";
 let imageUploadUrl = "";
 if (process.env.NODE_ENV === "dev") {
-  baseUrl = "http://39.104.102.187:7070";
+  baseUrl = "http://gr-debug.goodrain.com/";
 } else if (process.env.NODE_ENV === "development") {
   baseUrl = "http://39.104.102.187:7070";
 } else if (process.env.NODE_ENV === "production") {
