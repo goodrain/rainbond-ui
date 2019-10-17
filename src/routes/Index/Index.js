96cb18456831c0dd03b79a07f83fb212c33867aa
zsl <>
2019-10-17 20:09:34 +0800

    [REV] Modify the desc

--- src/routes/Index/Index.js
+++ src/routes/Index/Index.js
@@ -473,7 +473,6 @@ export default class Index extends PureComponent {
                     {item.service_name}
                   </Link>
                 )}
-                <span>应用</span>
                 <span
                   style={{
                     color: globalUtil.fetchAbnormalcolor(OptType)
