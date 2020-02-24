a50e853e1452db15e2fd70b3b22d77be4b0fe0e5
barnettZQG <zengqg@goodrain.com>
2020-02-24 21:29:25 +0800

    change node version

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 HOME=/home/travis/build/goodrain/rainbond-ui
-docker run -it --rm -v "$(pwd)":${HOME} -w=${HOME} node:10 yarn install && yarn run build
+docker run -it --rm -v "$(pwd)":${HOME} -w=${HOME} node:12 yarn install && yarn run build
+
+if [ ! -d "./dist" ];then
+    exit 1;
+fi
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then ./release.sh ;fi
