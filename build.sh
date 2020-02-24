6f0a46542a5a7e9923e8210d7064f29485b857ab
barnettZQG <zengqg@goodrain.com>
2020-02-24 21:22:38 +0800

    change build shell

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+HOME=/home/travis/build/goodrain/rainbond-ui
+docker run -it --rm -v "$(pwd)":${HOME} -w=${HOME} node:10 yarn install && yarn run build
 
-docker run -it --rm -v "$(pwd)":/app -w=/app node:10 yarn install && yarn run build
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then ./release.sh ;fi
