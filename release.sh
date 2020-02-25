5065757a441f47225d0fdf74951e17d9f4fb2113
barnettZQG <zengqg@goodrain.com>
2020-02-25 12:31:11 +0800

    support DOMESTIC_BASE_NAME

--- release.sh
+++ release.sh
@@ -9,7 +9,16 @@ fi
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker build -t "rainbond/rainbond-ui:$VERSION" .
 docker push "rainbond/rainbond-ui:$VERSION"
+
 sed -i "s/VERSION/$VERSION/g" ./build/Dockerfile
 mv dist build/dist
 docker build -t "rainbond/rbd-app-ui:$VERSION" ./build
 docker push "rainbond/rbd-app-ui:$VERSION"
+
+docker tag "rainbond/rbd-app-ui:$VERSION" 
+if [ ${DOMESTIC_BASE_NAME} ];
+then
+	docker tag "rainbond/rbd-app-ui:$VERSION" "${DOMESTIC_BASE_NAME}/${DOMESTIC_NAMESPACE}/rbd-app-ui:${VERSION}"
+	docker login -u "$DOMESTIC_DOCKER_USERNAME" -p "$DOMESTIC_DOCKER_PASSWORD" ${DOMESTIC_BASE_NAME}
+	docker push "${DOMESTIC_BASE_NAME}/${DOMESTIC_NAMESPACE}/rbd-$1:${VERSION}"
+fi
