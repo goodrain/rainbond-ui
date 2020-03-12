81982b04881be83866172fec35d51599f991e104
barnettZQG <zengqg@goodrain.com>
2020-03-12 13:44:25 +0800

    change release

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-if [ -z "$TRAVIS_TAG" ]; then
-	VERSION=$TRAVIS_BRANCH-dev
-else
-	VERSION=$TRAVIS_TAG
+if [ -z "$VERSION" ];then
+	if [ -z "$TRAVIS_TAG" ]; then
+		VERSION=$TRAVIS_BRANCH-dev
+	else
+		VERSION=$TRAVIS_TAG
+	fi
 fi
-
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker build -t "rainbond/rainbond-ui:$VERSION" .
 docker push "rainbond/rainbond-ui:$VERSION"
