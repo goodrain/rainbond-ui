89345c26d6e2a2ecf2641616efcf2744166f2dc2
barnettZQG <zengqg@goodrain.com>
2020-03-14 17:36:35 +0800

    support code build

--- config/api.config.js
+++ config/api.config.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === "dev") {
 } else if (process.env.NODE_ENV === "development") {
   baseUrl = "http://39.104.15.64:7070";
 } else if (process.env.NODE_ENV === "production") {
-  baseUrl = "";
+  baseUrl = "http://5000.gre13715.2c9v614j.17f4cc.grapps.cn";
 }
 imageUploadUrl = `${baseUrl}/console/files/upload`;
 const config = {
--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ if [ -z "$VERSION" ];then
 fi
 DOMESTIC_BASE_NAME="image.goodrain.com"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin  ${DOMESTIC_BASE_NAME}
-docker build -t "$DOMESTIC_BASE_NAME/rainbond-ui:$VERSION" .
+docker build -t "$DOMESTIC_BASE_NAME/rainbond-ui:$VERSION" -f Dockerfile.release .
 docker push "$DOMESTIC_BASE_NAME/rainbond-ui:$VERSION"
 
 sed -i "s/VERSION/$VERSION/g" ./build/Dockerfile
