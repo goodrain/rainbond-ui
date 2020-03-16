c62f1d098eb2cb0fb71aa49a3146259c7ce9348c
barnettZQG <zengqg@goodrain.com>
2020-03-16 14:42:04 +0800

    add web.conf

--- config/api.config.js
+++ config/api.config.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === "dev") {
 } else if (process.env.NODE_ENV === "development") {
   baseUrl = "http://39.104.15.64:7070";
 } else if (process.env.NODE_ENV === "production") {
-  baseUrl = process.env.API_DOMAIN;
+  baseUrl = "/";
 }
 imageUploadUrl = `${baseUrl}/console/files/upload`;
 const config = {
