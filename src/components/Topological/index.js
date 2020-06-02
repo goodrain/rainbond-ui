320f37f89072594b7419c78edf4fb03a2e6801e4
zsl <>
2020-06-02 19:22:46 +0800

    [REV] Modify the region_name

--- src/components/Topological/index.js
+++ src/components/Topological/index.js
@@ -10,79 +10,88 @@ class Index extends React.Component {
   constructor(props) {
     super(props);
   }
-  componentDidMount() {
-
-  }
+  componentDidMount() {}
   componentWillMount() {
     const team_name = globalUtil.getCurrTeamName();
     const groupId = this.props.group_id;
-    var self = this;
+    const self = this;
     try {
+      window.iframeGetNodeUrl = function() {
+        return `${
+          apiconfig.baseUrl
+        }/console/teams/${team_name}/topological?group_id=${groupId}&region_name=${globalUtil.getCurrRegionName()}`;
+      };
 
-      window.iframeGetNodeUrl = function () {
-        return apiconfig.baseUrl + '/console/teams/' + team_name + '/topological?group_id=' + groupId + '&region=' + globalUtil.getCurrRegionName();
-      }
-
-      window.iframeGetMonitor = function (fn) {
+      window.iframeGetMonitor = function(fn) {
         self.props.dispatch({
           type: 'groupControl/groupMonitorData',
           payload: {
             team_name: globalUtil.getCurrTeamName(),
-            group_id: groupId
+            group_id: groupId,
           },
-          callback: (data) => {
+          callback: data => {
             if (data) {
-              fn && fn(data || {})
+              fn && fn(data || {});
             }
-          }
-        })
+          },
+        });
 
-        return apiconfig.baseUrl + '/console/teams/' + team_name + '/topological?group_id=' + groupId + '&region=' + globalUtil.getCurrRegionName();
-      }
+        return `${
+          apiconfig.baseUrl
+        }/console/teams/${team_name}/topological?group_id=${groupId}&region_name=${globalUtil.getCurrRegionName()}`;
+      };
 
-      window.iframeGetTenantName = function () {
+      window.iframeGetTenantName = function() {
         return team_name;
-      }
+      };
 
-      window.iframeGetRegion = function () {
+      window.iframeGetRegion = function() {
         return globalUtil.getCurrRegionName();
-      }
+      };
 
-      window.iframeGetGroupId = function () {
+      window.iframeGetGroupId = function() {
         return groupId;
-      }
+      };
 
-      //拓扑图点击服务事件
-      window.handleClickService = function (nodeDetails) {
-        self
-          .props
-          .dispatch(routerRedux.push(`/team/${globalUtil.getCurrTeamName()}/region/${globalUtil.getCurrRegionName()}/components/${nodeDetails.service_alias}/overview`))
-      }
+      // 拓扑图点击服务事件
+      window.handleClickService = function(nodeDetails) {
+        self.props.dispatch(
+          routerRedux.push(
+            `/team/${globalUtil.getCurrTeamName()}/region/${globalUtil.getCurrRegionName()}/components/${
+              nodeDetails.service_alias
+            }/overview`
+          )
+        );
+      };
 
-      //拓扑图点击依赖服务事件
-      window.handleClickRelation = function (relation) {
-        self
-          .props
-          .dispatch(routerRedux.push(`/team/${globalUtil.getCurrTeamName()}/region/${globalUtil.getCurrRegionName()}/components/${relation.service_alias}/overview`))
-      }
-
-    } catch (e) { }
+      // 拓扑图点击依赖服务事件
+      window.handleClickRelation = function(relation) {
+        self.props.dispatch(
+          routerRedux.push(
+            `/team/${globalUtil.getCurrTeamName()}/region/${globalUtil.getCurrRegionName()}/components/${
+              relation.service_alias
+            }/overview`
+          )
+        );
+      };
+    } catch (e) {}
   }
   render() {
-    return ((
+    return (
       <iframe
-        src={apiconfig.baseUrl + '/static/www/weavescope/index.html'}
+        src={`${apiconfig.baseUrl}/static/www/weavescope/index.html`}
         style={{
           width: '100%',
-          height: '500px'
+          height: '500px',
         }}
         sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
         scrolling="auto"
         frameBorder="no"
         border="0"
         marginWidth="0"
-        marginHeight="0"></iframe>
-    ));
+        marginHeight="0"
+      />
+    );
   }
 }
 
