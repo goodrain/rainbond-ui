3d80328f8bd3c577ebf9fc705550a1e25f18a234
zsl <>
2020-07-20 16:14:05 +0800

    Modify the  version

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir ./node_modules
 chmod 777 ./node_modules
-docker run -it --rm -v "$(pwd)":/app -w=/app node:12 yarn install && yarn run build
+docker run -it --rm -v "$(pwd)":/app -w=/app node:12.18.2 yarn install && yarn run build
 
 if [ ! -d "./dist" ];then
     exit 1;
