c2294b427ef625e2e77aebc83b2321dd36aa4bc2
zsl1549 <zsl1549@163.com>
2020-09-11 11:23:55 +0800

    Add the BusinessMonitoring function

--- src/services/monitor.js
+++ src/services/monitor.js
@@ -1,49 +1,33 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-undef */
-import apiconfig from "../../config/api.config";
-import request from "../utils/request";
+import apiconfig from '../../config/api.config';
+import request from '../utils/request';
 
 /*
 	获取应用在线人数监控数据(当前请求时间点的数据)
 */
-export function getMonitorRangeData(
-  body = {
-    teamName,
-    componentAlias,
-    serviceID,
-  }
-) {
+export function getMonitorRangeData(body = {}) {
   return request(
-    `${apiconfig.baseUrl}/console/teams/${body.teamName}/apps/${
-      body.componentAlias
-    }/monitor/query_range`,
+    `${apiconfig.baseUrl}/console/teams/${body.teamName}/apps/${body.componentAlias}/monitor/query_range`,
     {
-      method: "get",
+      method: 'get',
       showMessage: false,
       params: {
         query: body.query,
-        start: body.start || (new Date().getTime() / 1000 - 60*60),
+        start: body.start || new Date().getTime() / 1000 - 60 * 60,
         end: body.end || new Date().getTime() / 1000,
         step: body.step || 72
       },
-      showLoading: false,
+      showLoading: false
     }
   );
 }
 
-export function getComponentCPURange(
-  body = {
-    teamName,
-    componentAlias,
-    serviceId
-  }
-) {
+export function getComponentCPURange(body = {}) {
   return request(
-    `${apiconfig.baseUrl}/console/teams/${body.team_name}/apps/${
-      body.app_alias
-    }/monitor/query`,
+    `${apiconfig.baseUrl}/console/teams/${body.team_name}/apps/${body.app_alias}/monitor/query`,
     {
-      method: "get",
+      method: 'get',
       showMessage: false,
       params: {
         query: `max(app_requestclient{service_id="${body.serviceId}"})`
@@ -53,24 +37,53 @@ export function getComponentCPURange(
   );
 }
 
-export function getComponentNetworkRange(
-  body = {
-    teamName,
-    componentAlias,
-    serviceId
-  }
-) {
+export function getComponentNetworkRange(body = {}) {
   return request(
-    `${apiconfig.baseUrl}/console/teams/${body.team_name}/apps/${
-      body.app_alias
-    }/monitor/query`,
+    `${apiconfig.baseUrl}/console/teams/${body.team_name}/apps/${body.app_alias}/monitor/query`,
     {
-      method: "get",
+      method: 'get',
       showMessage: false,
       params: {
         query: `max(app_requestclient{service_id="${body.serviceId}"})`
       },
       showLoading: false
+    }
+  );
+}
+
+export function getServiceMonitor(body = {}) {
+  return request(
+    `${apiconfig.baseUrl}/console/teams/${body.team_name}/apps/${body.app_alias}/service_monitor`,
+    {
+      method: 'get',
+      params: body
+    }
+  );
+}
+export async function postServiceMonitor(params) {
+  return request(
+    `${apiconfig.baseUrl}/console/teams/${params.team_name}/apps/${params.app_alias}/service_monitor`,
+    {
+      method: 'post',
+      data: params
+    }
+  );
+}
+
+export async function deleteServiceMonitor(params) {
+  return request(
+    `${apiconfig.baseUrl}/console/teams/${params.team_name}/apps/${params.app_alias}/service_monitor/${params.name}`,
+    {
+      method: 'delete'
+    }
+  );
+}
+export async function updateServiceMonitor(body) {
+  return request(
+    `${apiconfig.baseUrl}/console/teams/${body.team_name}/apps/${body.app_alias}/service_monitor/${body.name}`,
+    {
+      method: 'put',
+      data: body
     }
   );
 }
