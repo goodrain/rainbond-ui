7c3ded891d2153ff0bbe9ef37cd7b1187fd70364
zsl1549 <zsl1549@163.com>
2020-10-23 14:40:56 +0800

    Fix bug : Parameter calibration

--- config/api.config.js
+++ config/api.config.js
@@ -3,13 +3,13 @@ let imageUploadUrl = '';
 if (process.env.NODE_ENV === 'dev') {
   // baseUrl = "http://gr-debug.goodrain.com/";
 } else if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'https://goodrain.pre.goodrain.com';
+  baseUrl = 'https://goodrain.goodrain.com';
 } else if (process.env.NODE_ENV === 'production') {
   baseUrl = '';
 }
 imageUploadUrl = `${baseUrl}/console/files/upload`;
 const config = {
   baseUrl,
-  imageUploadUrl,
+  imageUploadUrl
 };
 export default config;
