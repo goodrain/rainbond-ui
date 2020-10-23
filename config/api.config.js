484d46582c03b0029bd2e77c8f2972caf79415fe
zsl1549 <32888522+zsl1549@users.noreply.github.com>
2020-10-23 14:42:43 +0800

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -3,7 +3,7 @@ let imageUploadUrl = '';
 if (process.env.NODE_ENV === 'dev') {
   // baseUrl = "http://gr-debug.goodrain.com/";
 } else if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'https://goodrain.goodrain.com';
+  baseUrl = 'https://goodrain.pre.goodrain.com';
 } else if (process.env.NODE_ENV === 'production') {
   baseUrl = '';
 }
