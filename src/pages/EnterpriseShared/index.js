2ca23107ca5f592adb4d31961fda044eb5d0693a
zsl1549 <zsl1549@163.com>
2020-12-01 22:50:04 +0800

    Fix bug  Application market edit Application template add category tag save not effective

--- src/pages/EnterpriseShared/index.js
+++ src/pages/EnterpriseShared/index.js
@@ -822,10 +822,10 @@ export default class EnterpriseShared extends PureComponent {
                   >
                     <Col span={3} style={{ display: 'flex' }}>
                       <div className={styles.lt}>
-                        <p>
+                        <div>
                           <Icon type="arrow-down" />
-                          {installNumber}
-                        </p>
+                          <div title={installNumber}>{installNumber}</div>
+                        </div>
                       </div>
                       <div className={styles.imgs}>
                         {pic ? <img src={pic} alt="" /> : defaulAppImg}
@@ -850,7 +850,7 @@ export default class EnterpriseShared extends PureComponent {
                         </p>
                       </div>
                     </Col>
-                    <Col span={4} className={styles.status}>
+                    <Col span={3} className={styles.status}>
                       <div>
                         {devStatus && (
                           <p className={styles.dev_status}>{devStatus}</p>
@@ -865,7 +865,7 @@ export default class EnterpriseShared extends PureComponent {
                         )}
                       </div>
                     </Col>
-                    <Col span={4} className={styles.tags}>
+                    <Col span={5} className={styles.tags}>
                       {tags &&
                         tags.length > 0 &&
                         tags.map((item, index) => {
@@ -874,7 +874,11 @@ export default class EnterpriseShared extends PureComponent {
                             return null;
                           }
                           return (
-                            <div key={tagId} style={{ marginRight: '5px' }}>
+                            <div
+                              key={tagId}
+                              title={name}
+                              style={{ marginRight: '5px' }}
+                            >
                               {name}
                             </div>
                           );
@@ -882,8 +886,10 @@ export default class EnterpriseShared extends PureComponent {
                       {tags && tags.length > 3 && (
                         <a
                           style={{ marginLeft: '5px' }}
-                          onClick={() => {
-                            this.handleOpenMoreTags(tags);
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            const customTags = tags.map((item) => item.name);
+                            this.handleOpenMoreTags(customTags);
                           }}
                         >
                           更多
@@ -987,10 +993,10 @@ export default class EnterpriseShared extends PureComponent {
                   <div className={styles.h70}>
                     <Col span={3} style={{ display: 'flex' }}>
                       <div className={styles.lt}>
-                        <p>
+                        <div>
                           <Icon type="arrow-down" />
-                          {installNumber}
-                        </p>
+                          <div title={installNumber}>{installNumber}</div>
+                        </div>
                       </div>
                       <div className={styles.imgs}>
                         {logo ? <img src={logo} alt="" /> : defaulAppImg}
@@ -1014,7 +1020,7 @@ export default class EnterpriseShared extends PureComponent {
                         </p>
                       </div>
                     </Col>
-                    <Col span={4} className={styles.status}>
+                    <Col span={3} className={styles.status}>
                       <div>
                         {devStatus && (
                           <p className={styles.dev_status}>{devStatus}</p>
@@ -1029,7 +1035,7 @@ export default class EnterpriseShared extends PureComponent {
                         )}
                       </div>
                     </Col>
-                    <Col span={4} className={styles.tags}>
+                    <Col span={5} className={styles.tags}>
                       {tags &&
                         tags.length > 0 &&
                         tags.map((item, index) => {
@@ -1037,7 +1043,11 @@ export default class EnterpriseShared extends PureComponent {
                             return null;
                           }
                           return (
-                            <div key={item} style={{ marginRight: '5px' }}>
+                            <div
+                              key={item}
+                              title={item}
+                              style={{ marginRight: '5px' }}
+                            >
                               {item}
                             </div>
                           );
