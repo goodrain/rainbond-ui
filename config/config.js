bf14db97147f5b593c35200c8a05ba3b5bf3e431
liuk <liukai72709601all@163.com>
2021-05-10 19:00:37 +0800

    Fix the Add an interface to the app overview page to get the number of apps to update

--- config/config.js
+++ config/config.js
@@ -47,11 +47,11 @@ export default {
   routes: routerConfig,
   proxy: {
     '/console': {
-      target: 'http://4000.gr7c60a0.2c9v614j.17f4cc.grapps.cn',
+      target: '127.0.0.1:8000',
       changeOrigin: true
     },
     '/data': {
-      target: 'http://4000.gr7c60a0.2c9v614j.17f4cc.grapps.cn',
+      target: '127.0.0.1:7070',
       changeOrigin: true
     }
   }
