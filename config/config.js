7f21ff8086ab7cd360f836c77173ad957796cc53
liuk <liukai72709601all@163.com>
2021-05-19 16:58:45 +0800

    The optimization of the code is flawed

--- config/config.js
+++ config/config.js
@@ -47,11 +47,11 @@ export default {
   routes: routerConfig,
   proxy: {
     '/console': {
-      target: 'http://4000.greb482e.mehc4gqk.ad2eea.grapps.cn/',
+      target: 'http://127.0.0.1:7070',
       changeOrigin: true
     },
     '/data': {
-      target: 'http://4000.greb482e.mehc4gqk.ad2eea.grapps.cn/',
+      target: 'http://127.0.0.1:7070',
       changeOrigin: true
     }
   }
