b76e991e11b231b7aca044dd000c13f32e368e72
zsl1549 <zsl1549@163.com>
2021-05-21 14:17:46 +0800

    Fix bug the  Instance clicks cannot jump

--- src/components/AppInstanceList/index.js
+++ src/components/AppInstanceList/index.js
@@ -1,46 +1,47 @@
 /* eslint-disable react/sort-comp */
 /* eslint-disable no-unused-expressions */
-import { List } from "antd";
-import React, { PureComponent } from "react";
-import WaterWave from "../Charts/WaterWave";
-import style from "./index.less";
+import { List } from 'antd';
+import React, { PureComponent } from 'react';
+import WaterWave from '../Charts/WaterWave';
+import style from './index.less';
 
 class InstanceList extends PureComponent {
   constructor(arg) {
     super(arg);
     this.state = {
-      list: this.props.list,
-    }
+      list: this.props.list
+    };
   }
-  componentWillReceiveProps(nextProps){
-    if(nextProps.list!==this.props.list){
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.list !== this.props.list) {
       this.setState({
-        list:nextProps.list
-      })
+        list: nextProps.list
+      });
     }
   }
   componentDidMount() {}
-  showName = (podName) => {
-    const num = podName.split("-")[1];
+  showName = podName => {
+    const num = podName.split('-')[1];
     return `实例${num}`;
   };
-  getContainerMem = (containers)=> {
-    let memRate = 0
-    containers && containers.map(c=>{
-      if (c.container_name === this.props.serviceID) {
-        memRate = c.usage_rate
-      }
-    })
-    return memRate
-  }
+  getContainerMem = containers => {
+    let memRate = 0;
+    containers &&
+      containers.map(c => {
+        if (c.container_name === this.props.serviceID) {
+          memRate = c.usage_rate;
+        }
+      });
+    return memRate;
+  };
   render() {
     const statusObj = {
-      "Running":"正常运行",
-      "Pending":"启动中",
-      "Succeeded":"运行成功",
-      "Failed":"运行失败",
-      "Unknown":"未知",
-    }
+      Running: '正常运行',
+      Pending: '启动中',
+      Succeeded: '运行成功',
+      Failed: '运行失败',
+      Unknown: '未知'
+    };
     return (
       <List
         grid={{ gutter: 16, column: 4 }}
@@ -53,13 +54,8 @@ class InstanceList extends PureComponent {
               title="运行内存"
               percent={this.getContainerMem(item.container)}
             />
-             <a className={style.instancename} href="javascript:;">
-              {this.showName(item.pod_name)}
-            </a>
-            <br />
-            <a href="javascript:;" style={{ color: '#000' }}>
-              {statusObj[item.pod_status]}
-            </a>
+            <div>{this.showName(item.pod_name)}</div>
+            <div>{statusObj[item.pod_status]}</div>
           </List.Item>
         )}
       />
