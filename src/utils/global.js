39a34a16fe3ef37a5dda0e543a1b65d2b6004c66
RainBondsongyg <song76813995@163.com>
2022-07-15 16:26:09 +0800

    fix: status color

--- src/utils/global.js
+++ src/utils/global.js
@@ -242,7 +242,7 @@ const global = {
       Some_abnormal: '#FF0000', // 一些不正常 纯红
       Building: '#007710', // 构建  纯蓝
       succeeded: '#00D777', 
-      SUCCEEDED:'#6db7fb' //蓝色 已完成
+      SUCCEEDED:'#00D777' //蓝色 已完成
     };
     return statusColorMap[status] || statusColorMap.unknow;
   },
