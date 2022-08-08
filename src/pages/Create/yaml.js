2efe350d9323cf2823dd8edc1d5ecc7c5c376109
guox <guox@MacBook-Pro-For-Guox.local>
2022-08-08 15:12:26 +0800

    fix: fix build source bugs

--- src/pages/Create/yaml.js
+++ src/pages/Create/yaml.js
@@ -53,20 +53,6 @@ export default class Index extends PureComponent {
   }
   componentDidMount() {
     this.handleJarWarUploadRecord('jwar')
-    const teamName = globalUtil.getCurrTeamName();
-    const { form, dispatch } = this.props;
-    dispatch({
-      type: "teamControl/getUploadInformation",
-      payload: {
-        team_name:teamName,
-          event_id: '123456789',
-          group_id: '123',
-      },
-      callback: (data) => {
-        console.log(data,"data");
-      },
-    });
-
   }
   componentWillUnmount() {
     this.loop = false;
@@ -87,14 +73,6 @@ export default class Index extends PureComponent {
             `/team/${teamName}/region/${regionName}/importMessageYaml?event_id=${event_id}&group_id=${value.group_id}`
           )
         );
-        // getUploadInformation({
-        //   team_name:teamName,
-        //   event_id: '123456789',
-        //   group_id: '123',
-        // }).then( res =>{
-        //   console.log(res,"res");
-        // })
-        
       } else if (value.up_type === 'jwar' && existFileList.length > 0) {
         dispatch({
           type: "createApp/createJarWarFormSubmit",
