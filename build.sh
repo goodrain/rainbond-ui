e1cb8471161b42c1d6f3240a6040e2a4f4ed1ce3
张启航 <101104760+ZhangSetSail@users.noreply.github.com>
2022-11-03 21:49:01 +0800

    feat: upgrade node.js version (#1083)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir ./node_modules
 chmod 777 ./node_modules
-docker run --rm -v "$(pwd)":/app -w=/app node:12 sh -c "yarn install && yarn run build"
+docker run --rm -v "$(pwd)":/app -w=/app node:14 sh -c "yarn install && yarn run build"
 
 if [ ! -d "./dist" ];then
     exit 1;
