92bd358c0305e503c2dc2d1415e2f10d86277789
RainBondsongyg <song76813995@163.com>
2022-11-07 13:22:21 +0800

    fix: Platform first registration jump adjustment

--- src/pages/User/RegisterResult.js
+++ src/pages/User/RegisterResult.js
@@ -1,28 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import { routerRedux } from 'dva/router';
 import { formatMessage, FormattedMessage } from 'umi-plugin-locale';
 import { Button } from 'antd';
 import Result from '../../components/Result';
 import styles from './RegisterResult.less';
+@connect(({ user, global }) => ({
+  register: user.register,
+  rainbondInfo: global.rainbondInfo,
+  isRegist: global.isRegist
+}))
 
-const actions = (
-  <div className={styles.actions}>
-    <a href="/">
-      <Button size="large"><FormattedMessage id="login.RegisterResult.back"/></Button>
-    </a>
-  </div>
-);
-
-export default ({ location }) => (
-  <Result
-    className={styles.registerResult}
-    type="success"
-    title={
-      <div className={styles.title}>
-        <FormattedMessage id="login.RegisterResult.your"/>{location.state ? location.state.account : 'xxx'} <FormattedMessage id="login.RegisterResult.success"/>
-      </div>
+export default class Register extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      eid:'',
+      is_admin: 0
     }
-    description=""
-    actions={actions}
-    style={{ marginTop: 172 }}
-  />
-);
+  }
+  componentDidMount(){
+    this.getEnterpriseList()
+  }
+   // 获取企业列表
+  getEnterpriseList = () => {
+    const { dispatch } = this.props;
+    
+    dispatch({
+      type: 'global/fetchEnterpriseList',
+      callback: res => {
+        if(res.list.length > 0){
+          this.setState({
+            eid:res.list[0].enterprise_id
+          },()=>{
+            const { eid } = this.state
+            this.loadUser(eid)
+          })
+
+        }
+      },
+      handleError: () => {
+      }
+    });
+  };
+  loadUser = (eid) => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'global/fetchEnterpriseUsers',
+      payload: {
+        enterprise_id: eid,
+        page: 1,
+        page_size: 10,
+        name: ''
+      },
+      callback: res => {
+        if (res) {
+          const is_admin = res.list.length
+          this.setState({
+            is_admin
+          })
+        }
+      }
+    });
+  };
+  onRouterLink = (eid, is_admin)=>{
+    const { dispatch } = this.props;
+    if(is_admin == 1){
+      dispatch(routerRedux.replace(`/enterprise/${eid}/index`))
+    }else{
+      dispatch(routerRedux.replace(`/enterprise/${eid}/personal`))
+    }
+    
+  }
+  render() {
+    const { location,user } = this.props
+    const { eid, is_admin } = this.state
+    const actions = (
+      <div className={styles.actions}>
+        {is_admin &&<Button size="large" onClick={()=>{this.onRouterLink(eid,is_admin)}}><FormattedMessage id="login.RegisterResult.back" /></Button>}
+      </div>
+    );
+    return (
+      <Result
+        className={styles.registerResult}
+        type="success"
+        title={
+          <div className={styles.title}>
+
+            <FormattedMessage id="login.RegisterResult.your" />{location.state ? location.state.account : 'xxx'} <FormattedMessage id="login.RegisterResult.success" />
+          </div>
+        }
+        description=""
+        actions={actions}
+        style={{ marginTop: 172 }}
+      />
+    )
+  }
+
+}
