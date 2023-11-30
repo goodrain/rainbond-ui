402588bf83f59be813fd896c213def5bc6a85762
xuzl <xuzl@goodrain.com>
2023-11-30 16:32:26 +0800

    fix: The host field is empty

--- config/config.js
+++ config/config.js
@@ -49,11 +49,11 @@ export default {
   routes: routerConfig,
   proxy: {
     '/console': {
-      target: 'http://127.0.0.1:8000',
+      target: 'http://8.147.115.134:7070/',
       changeOrigin: true
     },
     '/data': {
-      target: 'http://127.0.0.1:8000',
+      target: 'http://8.147.115.134:7070/',
       changeOrigin: true
     }
   }
