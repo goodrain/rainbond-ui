f0ee8f439c3c9182a9328b4d0741c2ff819170ad
songyg <songyg@goodrain.com>
2024-03-15 15:20:46 +0800

    fix: java maven cmd

--- src/components/CodeBuildConfig/java-maven/index.js
+++ src/components/CodeBuildConfig/java-maven/index.js
@@ -263,7 +263,7 @@ class Index extends PureComponent {
         </Form.Item>
         <Form.Item {...formItemLayout}  label={<FormattedMessage id='componentOverview.body.JavaMavenConfig.start'/>}>
           {getFieldDecorator('BUILD_PROCFILE', {
-            initialValue: (envs && envs.BUILD_PROCFILE) || 'web: java $JAVA_OPTS -jar ./webapp-runner.jar --port $PORT ./*.war',
+            initialValue: (envs && envs.BUILD_PROCFILE) || 'web: java $JAVA_OPTS -jar target/*.jar',
             rules: [
               {
                 required: true,
@@ -271,7 +271,7 @@ class Index extends PureComponent {
               }
             ]
           })(
-            <Input placeholder="web: java $JAVA_OPTS -jar ./webapp-runner.jar --port $PORT ./*.war" />
+            <Input placeholder="web: java $JAVA_OPTS -jar target/*.jar" />
           )}
         </Form.Item>
       </div>
