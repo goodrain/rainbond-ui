b03799de0248e96b4774bf3901a1632f2ba665ed
Qi Zhang <smallqi1@163.com>
2024-10-16 14:59:09 +0800

    fix: add script and modify self host install parms (#1388)
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- src/components/Cluster/RainbondClusterInit/index.js
+++ src/components/Cluster/RainbondClusterInit/index.js
@@ -147,7 +147,7 @@ export default class RainbondClusterInit extends PureComponent {
     form.validateFields((err, values) => {
       let dataObj = {
         useK3sContainerd: true,
-        Component:{rbd_app_ui:{enable: true}},
+        Component:{rbd_app_ui:{enable: false}},
         operator: {
           env: [
             {
