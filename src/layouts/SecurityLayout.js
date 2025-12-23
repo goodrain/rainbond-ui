6221333b4d09c30c7510025f61c87ee2e12066a8
songyg <songyg@goodrain.com>
2025-12-23 18:20:48 +0800

    fix: token login

--- src/layouts/SecurityLayout.js
+++ src/layouts/SecurityLayout.js
@@ -13,6 +13,9 @@ class SecurityLayout extends React.PureComponent {
   };
 
   componentDidMount() {
+    // 检查URL中是否有门户传来的token参数，如果有则设置到cookie
+    this.checkAndSetPortalToken();
+
     const { dispatch } = this.props;
     if (dispatch) {
       dispatch({
@@ -23,6 +26,23 @@ class SecurityLayout extends React.PureComponent {
           }
         },
       });
+    }
+  }
+
+  checkAndSetPortalToken = () => {
+    // 从URL参数中获取token
+    const urlParams = new URLSearchParams(window.location.search);
+    const portalToken = urlParams.get('token');
+
+    if (portalToken) {
+      // 如果URL中有token，设置到cookie中
+      cookie.set('token', portalToken);
+
+      // 清除URL中的token参数，避免token暴露在地址栏
+      urlParams.delete('token');
+      const newSearch = urlParams.toString();
+      const newUrl = `${window.location.pathname}${newSearch ? '?' + newSearch : ''}${window.location.hash}`;
+      window.history.replaceState(null, '', newUrl);
     }
   }
 
