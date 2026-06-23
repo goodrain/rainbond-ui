b24b2204f2628990db236e3ac87f2bfe948302e4
Qi Zhang <smallqi1@163.com>
2026-06-23 19:41:53 +0800

    fix: show jar war package build config

--- src/pages/Component/resourceHelpers.js
+++ src/pages/Component/resourceHelpers.js
@@ -1,7 +1,14 @@
 function shouldShowCodeBuildConfig({ buildSource, languageType, runtimeInfo }) {
+  const serviceSource = buildSource && buildSource.service_source;
+  const normalizedLanguage = (languageType || '').toLowerCase();
+  const isSourceCodeBuild = serviceSource === 'source_code';
+  const isPackageJavaArchiveBuild =
+    serviceSource === 'package_build' &&
+    (normalizedLanguage === 'java-jar' || normalizedLanguage === 'java-war');
+
   return !!(
     buildSource &&
-    buildSource.service_source === 'source_code' &&
+    (isSourceCodeBuild || isPackageJavaArchiveBuild) &&
     languageType &&
     runtimeInfo
   );
