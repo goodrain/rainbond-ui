ab03b28fe89aa33fe4e6538138809c36e91318f3
张启航 <101104760+ZhangSetSail@users.noreply.github.com>
2026-09-14 18:28:59 +0800

    fix: handle disabled login captcha (#1934)
    * feat: support login captcha
    
    * fix: handle disabled login captcha
    
    ---------
    
    Co-authored-by: 张启航 <12344192+zhangsetsail@user.noreply.gitee.com>

--- src/pages/User/loginComponent.js
+++ src/pages/User/loginComponent.js
@@ -5,16 +5,45 @@ import { connect } from 'dva';
 import { Link } from 'dva/router';
 import userUtil from '../../utils/global';
 import Login from '../../components/Login';
+import { getCaptchaURL } from '../../services/user';
 import styles from './Login.less';
 
-const { UserName, Password, Submit } = Login;
+const { UserName, Password, ImageCaptcha, Submit } = Login;
 
 @connect(({ loading, global }) => ({
   isRegist: global.isRegist,
+  rainbondInfo: global.rainbondInfo,
   thirdLogin: loading.effects['user/thirdLogin'],
   userLogin: loading.effects['user/login']
 }))
 export default class LoginComponent extends Component {
+  state = {
+    captchaVersion: Date.now()
+  };
+
+  componentDidUpdate(prevProps) {
+    const { userLogin } = this.props;
+    if (prevProps.userLogin && !userLogin && this.isCaptchaEnabled()) {
+      this.refreshCaptcha();
+    }
+  }
+
+  isCaptchaEnabled = () => {
+    const { rainbondInfo, type } = this.props;
+    return !!(
+      type !== 'thirdLogin' &&
+      rainbondInfo &&
+      rainbondInfo.captcha_code &&
+      rainbondInfo.captcha_code.enable
+    );
+  };
+
+  refreshCaptcha = () => {
+    this.setState(({ captchaVersion }) => ({
+      captchaVersion: captchaVersion + 1
+    }));
+  };
+
   handleSubmit = (err, values) => {
 
     const { onSubmit } = this.props;
@@ -41,6 +70,8 @@ export default class LoginComponent extends Component {
 
   render() {
     const { thirdLogin, userLogin, type } = this.props;
+    const { captchaVersion } = this.state;
+    const captchaEnabled = this.isCaptchaEnabled();
     const hiSvg = (
       <svg t="1766568075400" class="icon" viewBox="0 0 1066 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="43569" width="20" height="20"><path d="M879.701333 182.4l-2.346666-2.346667C694.442667-3.370667 397.525333-2.901333 214.144 180.053333s-182.912 480.426667 0 663.338667A467.285333 467.285333 0 0 0 546.218667 981.333333h349.866666a36.138667 36.138667 0 0 0 36.138667-36.138666V778.666667a487.168 487.168 0 0 0 60.501333-121.045334 470.613333 470.613333 0 0 0-113.066666-475.221333z m-373.802666 518.4l28.586666-152.021333H402.688l-29.056 152.021333H296.234667L365.226667 339.114667h77.397333l-26.752 141.226666h132.266667l27.221333-141.226666h77.354667l-68.949334 361.685333h-77.824z m224.170666 0h-77.397333l50.218667-262.698667h77.354666l-50.176 262.698667z m53.461334-306.816a42.24 42.24 0 0 1-32.341334 11.733333 35.669333 35.669333 0 0 1-37.546666-33.792V368.213333a43.946667 43.946667 0 0 1 44.544-44.117333 36.693333 36.693333 0 0 1 38.954666 34.261333v3.754667c0 11.733333-5.162667 23.466667-13.610666 31.914667z" fill="#155aef" p-id="43570"></path></svg>
     )
@@ -59,6 +90,30 @@ export default class LoginComponent extends Component {
             <label><FormattedMessage id="login.loginComponent.password.label" defaultMessage="密码" /></label>
             <Password name="password" placeholder={formatMessage({id:'login.loginComponent.pass'})}/>
           </div>
+          {captchaEnabled && (
+            <div className={styles.formItem}>
+              <label><FormattedMessage id="login.captcha.label" defaultMessage="验证码" /></label>
+              <div className={styles.captchaRow}>
+                <div className={styles.captchaInput}>
+                  <ImageCaptcha
+                    name="captcha_code"
+                    placeholder={formatMessage({ id: 'login.captcha.placeholder' })}
+                  />
+                </div>
+                <button
+                  type="button"
+                  className={styles.captchaRefresh}
+                  onClick={this.refreshCaptcha}
+                  aria-label={formatMessage({ id: 'login.captcha.refresh' })}
+                >
+                  <img
+                    src={getCaptchaURL(captchaVersion)}
+                    alt={formatMessage({ id: 'login.captcha.imageAlt' })}
+                  />
+                </button>
+              </div>
+            </div>
+          )}
           <Submit loading={type !== 'thirdLogin' ? userLogin : thirdLogin}>
             {type === 'thirdLogin' ? <FormattedMessage id="login.loginComponent.loginandband"/> : <FormattedMessage id="login.loginComponent.login"/>}
           </Submit>
